57db9b4f739c6a28dd9ebd6b8004db4b662761a6
Bastien Perez Assael <perez.bas@gmail.com>
2022-09-21 16:34:12 +0200

    zzz

--- app/javascript/plugins/flatpickr.js
+++ app/javascript/plugins/flatpickr.js
@@ -9,6 +9,7 @@ const initFlatpickr = () => {
   // Check that the query selector id matches the one you put around your form.
   if (startDateInput) {
   const unavailableDates = JSON.parse(document.querySelector('#car-booking-dates').dataset.unavailable)
+  console.log(unavailableDates)
   endDateInput.disabled = true
 
   flatpickr(startDateInput, {
@@ -29,7 +30,62 @@ const initFlatpickr = () => {
     dateFormat: "Y-m-d"
     });
   })
+
+  startDateInput.addEventListener("change", () => {
+    if (endDateInput.value != "") {
+      let pricePerDay = Number.parseInt(document.querySelector("#price_per_day").innerText)
+      let days = (Date.parse(endDateInput.value) - Date.parse(startDateInput.value)) / (1000*60*60*24)
+      if (days <= 0) {
+        document.querySelector('#booking-infos').innerText = "End date must be after start date"
+        document.querySelector('#submit-button').disabled = true
+      } else if (dateOverlap(unavailableDates, startDateInput.value, endDateInput.value)){
+        document.querySelector('#booking-infos').innerText = "These dates are not available"
+        document.querySelector('#submit-button').disabled = true
+      } else {
+        document.querySelector('#booking-infos').innerText = `Booking from ${startDateInput.value} to ${endDateInput.value} - ${days} days - Total price: $${days*pricePerDay}`
+        document.querySelector('#submit-button').disabled = false
+      }
+    }
+  })
+  endDateInput.addEventListener("change", () => {
+    if (startDateInput.value != "") {
+      let pricePerDay = Number.parseInt(document.querySelector("#price_per_day").innerText)
+      let days = (Date.parse(endDateInput.value) - Date.parse(startDateInput.value)) / (1000*60*60*24)
+      if (days <= 0) {
+        document.querySelector('#booking-infos').innerText = "End date must be after start date"
+        document.querySelector('#submit-button').disabled = true
+      } else if (dateOverlap(unavailableDates, startDateInput.value, endDateInput.value)){
+        document.querySelector('#booking-infos').innerText = "These dates are not available"
+        document.querySelector('#submit-button').disabled = true
+      } else {
+        document.querySelector('#booking-infos').innerText = `Booking from ${startDateInput.value} to ${endDateInput.value} - ${days} days - Total price: $${days*pricePerDay}`
+        document.querySelector('#submit-button').disabled = false
+      }
+    }
+  })
+
   };
+
+}
+
+
+const dateOverlap = (unavailable, start, end) => {
+  let overlap = false
+  unavailable.forEach((range) => {
+    if (dateBetween(start, end, range.from) || dateBetween(start, end, range.to)) {
+      overlap = true
+    }
+  })
+  return overlap
+}
+
+const dateBetween = (from, to, check) => {
+  let fDate,lDate,cDate;
+  fDate = Date.parse(from);
+  lDate = Date.parse(to);
+  cDate = Date.parse(check);
+  if((cDate <= lDate && cDate >= fDate))  return true
+  return false;
 }
 
 export { initFlatpickr };
